e04cb19dcfee1ec7e15235cf203a83c700d313a8
Dylan Tyler Hall <dthall43@students.tntech.edu>
2015-02-13 09:47:44 -0600

    Added All of our work.

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+
+set DRIVE_LETTER=g:
+set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;%DRIVE_LETTER%\MinGW\gtkmm3\bin;%DRIVE_LETTER%\MinGW\gtk\bin;c:\Windows;c:\Windows\system32
+set PROJECT_PATH=.
+
+make DRIVE_LETTER="%DRIVE_LETTER%" PROJECT_DIR="%PROJECT_PATH%"
+
+PAUSE
